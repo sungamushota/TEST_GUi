622f3214dd3096d37d7edc9d18f762eade221fdd
sungamushota <119276572+sungamushota@users.noreply.github.com>
2024-03-29 02:05:21 +0800

    initial commit

--- Open_window.py
+++ Open_window.py
@@ -28,7 +28,7 @@ class App(customtkinter.CTk):
         self.login_frame.grid(row=0, column=0, sticky="ns")
         self.login_label = customtkinter.CTkLabel(self.login_frame, text="CustomTkinter\nLogin Page",
                                                   font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.login_label.grid(row=0, column=0, padx=30, pady=(150, 15))
+        self.login_label.grid(row=0, column=0, padx=30, pady=(100, 15))
         self.username_entry = customtkinter.CTkEntry(self.login_frame, width=200, placeholder_text="username")
         self.username_entry.grid(row=1, column=0, padx=30, pady=(15, 15))
         self.password_entry = customtkinter.CTkEntry(self.login_frame, width=200, show="*", placeholder_text="password")
@@ -41,15 +41,24 @@ class App(customtkinter.CTk):
         #Create Test description Frame
         self.Description_frame = customtkinter.CTkFrame(self, corner_radius=0)
         self.Description_frame.grid_columnconfigure(0, weight=1)
-        self.Description_label = customtkinter.CTkLabel(self.Description_frame, text="CustomTkinter\nLogin Page",
+        self.Description_label = customtkinter.CTkLabel(self.Description_frame, text="Test Hardware Description\nSetup Page",
                                                   font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.Description_label.grid(row=0, column=0, padx=30, pady=(150, 15))
-        self.desc_username_entry = customtkinter.CTkEntry(self.Description_frame, width=200, placeholder_text="username")
-        self.desc_username_entry.grid(row=1, column=0, padx=30, pady=(15, 15))
-        self.desc_password_entry = customtkinter.CTkEntry(self.Description_frame, width=200, show="*", placeholder_text="password")
-        self.desc_password_entry.grid(row=2, column=0, padx=30, pady=(0, 15))
-        self.desc_login_button = customtkinter.CTkButton(self.Description_frame, text="Description", command=self.login_event, width=200)
-        self.desc_login_button.grid(row=3, column=0, padx=30, pady=(15, 15))
+        self.Description_label.grid(row=0, column=0, padx=30, pady=(50, 15))
+        self.desc_operator_entry = customtkinter.CTkEntry(self.Description_frame, width=200, placeholder_text="Operator Name")#username
+        self.desc_operator_entry.grid(row=1, column=0, padx=30, pady=(0, 15))
+        self.desc_department_entry = customtkinter.CTkEntry(self.Description_frame, width=200, placeholder_text="Department i.e. Engineering")#username
+        self.desc_department_entry.grid(row=2, column=0, padx=30, pady=(0, 15))
+        self.desc_hardware_entry = customtkinter.CTkEntry(self.Description_frame, width=200, placeholder_text="Hardware Description")#username
+        self.desc_hardware_entry.grid(row=3, column=0, padx=30, pady=(15, 15))
+        self.desc_serial_entry = customtkinter.CTkEntry(self.Description_frame, width=200, placeholder_text="Serial Number") 
+        self.desc_serial_entry.grid(row=4, column=0, padx=30, pady=(0, 15))
+        self.desc_assembly_entry = customtkinter.CTkEntry(self.Description_frame, width=200, placeholder_text="Assembly Number") #password #, show="*"
+        self.desc_assembly_entry.grid(row=5, column=0, padx=30, pady=(0, 15))
+
+        self.desc_login_button = customtkinter.CTkButton(self.Description_frame, text="Save Details", command=self.description_event, width=200)
+        self.desc_login_button.grid(row=6, column=0, padx=30, pady=(15, 15))
+
+        
 
         # create main frame
         self.main_frame = customtkinter.CTkFrame(self, corner_radius=0)
@@ -67,7 +76,13 @@ class App(customtkinter.CTk):
         self.main_frame.grid(row=0, column=0, sticky="nsew", padx=100)  # show main frame
         
     def description_event(self):
-        print("Login pressed - username:", self.username_entry.get(), "password:", self.password_entry.get())
+        print("Description pressed - Assembly:", self.desc_assembly_entry.get(), "Operator:", self.desc_operator_entry.get())
+        Operator  = self.desc_operator_entry
+        Assembly = self.desc_assembly_entry
+        serial = self.desc_serial_entry
+        hardware = self.desc_hardware_entry
+        department = self.desc_department_entry
+        
 
         self.login_frame.grid_forget()  # remove login frame
         self.Description_frame.grid(row=0, column=0, sticky="nsew", padx=100)  # show main frame
