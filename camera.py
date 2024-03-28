b968569739f485bc24173929a0823388159ade59
sungamushota <119276572+sungamushota@users.noreply.github.com>
2024-03-28 23:38:17 +0800

    initial commit

--- camera.py
+++ camera.py
@@ -1,6 +1,19 @@
 import cv2
+from matplotlib import pyplot as plt
 
-cam = cv2.VideoCapture(0)
-results, image = cam.read()
+def take_image():
+    print ("1")
+    cap = cv2.VideoCapture(0)
+    print ("2")
+    ret, frame = cap.read()
+    if ret:
+        print(ret)
+        #plt.imshow(frame)
+        cv2.imwrite("webcamphoto.jpg", frame)
+        cap.release()
 
+
+print ("okay")
+
+take_image()
 
